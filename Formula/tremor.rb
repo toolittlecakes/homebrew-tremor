789c6b3641eb6ae447e0080a6e160c1a9a0da4f3
sne <nickolay.sheyko@gmail.com>
2026-06-16 16:41:16 +0300

    tremor 0.5.0

--- Formula/tremor.rb
+++ Formula/tremor.rb
@@ -3,9 +3,9 @@ class Tremor < Formula
   homepage "https://github.com/toolittlecakes/tremor"
   # Built straight from the tagged source via git (no archive checksum to drift).
   url "https://github.com/toolittlecakes/tremor.git",
-      tag:      "v0.4.1",
-      revision: "988eddb80106f7fc7c49372b6b0b20159b328814"
-  version "0.4.1"
+      tag:      "v0.5.0",
+      revision: "e2440055d4938f43cb42da01acb323c2b4720fce"
+  version "0.5.0"
   license "MIT"
 
   depends_on :macos
